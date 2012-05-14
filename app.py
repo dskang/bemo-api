94e5fe4ead4f8eb800570c50f3d3dc0a54ee8815
Dan Kang <dan@dskang.com>
2012-05-14 17:16:52 -0400

    Get rid of debug flag

--- app.py
+++ app.py
@@ -528,6 +528,5 @@ if __name__ == "__main__":
         sys.exit(1)
 
     # Start the server
-    app.debug = True
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
